7476f7c3dc7f899a984506097eafd030effb2c92
Natalio Ruiz <natalio.ruiz@siriusxm.com>
2018-09-26 11:23:49 -0300

    uuid first commit

---
+++ activity/uuid/activity.go
@@ -0,0 +1,34 @@
+package uuid
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+	"github.com/google/uuid"
+)
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+
+	id := uuid.New()
+	logger.Info("Generated uuid: " + id.String())
+
+	// Set the output
+	context.SetOutput("uuid", id.String())
+
+	return true, nil
+}
---
+++ activity/uuid/activity_test.go
@@ -0,0 +1,61 @@
+package uuid
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/google/uuid"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+
+func TestEval(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	act.Eval(tc)
+	value := tc.GetOutput("uuid")
+
+	uuid, err := uuid.Parse(fmt.Sprintf("%v", value))
+	if err != nil {
+		t.Error("UUID cannot be parsed")
+	}
+
+	fmt.Printf("UUID Generated: %s", uuid.String())
+}
