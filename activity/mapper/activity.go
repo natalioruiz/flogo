1a1cc415cf94ee9a6f1acc049fd36040537bee67
Natalio Ruiz <natalio.ruiz@siriusxm.com>
2018-10-04 22:00:12 -0300

    adding object mapper

---
+++ activity/mapper/activity.go
@@ -0,0 +1,29 @@
+package uuid
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+)
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+
+	input := context.GetInput("input").(interface{})
+	context.SetOutput("output", input)
+
+	return true, nil
+}
---
+++ activity/mapper/activity_test.go
@@ -0,0 +1,64 @@
+package uuid
+
+import (
+	"io/ioutil"
+	"reflect"
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+
+func TestEval(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	input := map[string]interface{}{
+		"id1": "val1",
+		"id2": "val2",
+	}
+
+	tc.SetInput("input", input)
+	act.Eval(tc)
+
+	output := tc.GetOutput("output").(map[string]interface{})
+	if !reflect.DeepEqual(input, output) {
+		t.Error("Output is invalid.")
+	}
+
+}
